874fef9955fc1915a70aab4d1d4283325e56d892
franco zalazar <francorzalazar906@gmail.com>
2025-07-14 14:49:23 -0300

    3ero

---
+++ scripts.js
@@ -0,0 +1,76 @@
+// Carrusel
+let indice = 0;
+const slides = document.querySelectorAll('.slide');
+
+function cambiarSlide(direccion) {
+  const actual = slides[indice];
+  actual.classList.remove('active');
+  actual.classList.add('out');
+
+  indice = (indice + direccion + slides.length) % slides.length;
+  const siguiente = slides[indice];
+
+  setTimeout(() => {
+    actual.classList.remove('out');
+    siguiente.classList.add('active');
+  }, 500);
+}
+
+// Partículas de fondo
+const canvas = document.getElementById("particles");
+const ctx = canvas.getContext("2d");
+
+let particlesArray = [];
+const num = 80;
+
+function resizeCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+class Particle {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 2 + 1;
+    this.speedX = Math.random() * 0.5 - 0.25;
+    this.speedY = Math.random() * 0.5 - 0.25;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+
+    if (this.x < 0 || this.x > canvas.width) this.speedX *= -1;
+    if (this.y < 0 || this.y > canvas.height) this.speedY *= -1;
+  }
+
+  draw() {
+    ctx.fillStyle = "rgba(0,255,255,0.3)";
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+function initParticles() {
+  particlesArray = [];
+  for (let i = 0; i < num; i++) {
+    particlesArray.push(new Particle());
+  }
+}
+
+function animateParticles() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let p of particlesArray) {
+    p.update();
+    p.draw();
+  }
+  requestAnimationFrame(animateParticles);
+}
+
+initParticles();
+animateParticles();
